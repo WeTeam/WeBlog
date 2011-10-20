4e3dcbeb30ebf907e388b2b8e2f8dc4accb50fc4
Mark van Aalst <mva@sitecore.net>
2011-10-20 11:08:19 +0000

    Added TokenReplacer for using WeBlog.config setting values in templates ( example: $weblogsetting([SETTINGSNAME]) )

---
+++ Website/Pipelines/TokenReplacer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitecore.Pipelines.ExpandInitialFieldValue;
+using System.Text.RegularExpressions;
+
+namespace Sitecore.Modules.WeBlog.Pipelines
+{
+    public class TokenReplacer : ExpandInitialFieldValueProcessor
+    {
+        public override void Process(ExpandInitialFieldValueArgs args)
+        {
+            if (args.SourceField.Value.Contains("$weblogsetting"))
+            {
+                string originalValue = args.SourceField.Value;
+                Match match = Regex.Match(args.SourceField.Value, @"\(([^)]*)\)");
+
+                if (!string.IsNullOrEmpty(match.Value))
+                {
+                    string settingName = match.Value.Trim(new char[] { '(', ')' });
+                    string settingsValue = Sitecore.Configuration.Settings.GetSetting(settingName);
+
+                    if (!string.IsNullOrEmpty(settingsValue))
+                    {
+                        args.Result = originalValue.Replace(string.Format("$weblogsetting{0}", match.Value), settingsValue);
+                    }
+                }
+            }
+        }
+    }
+}
