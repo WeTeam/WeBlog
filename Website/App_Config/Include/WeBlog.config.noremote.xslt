50a699b5b8d01675cbdda2dc792ae2f2e98ad665
Alistair Deneys <alistair.deneys@codeflood.net>
2013-08-03 19:18:30 +0530

    Added guard for duplicate comment submission.

---
+++ Website/App_Config/Include/WeBlog.config.noremote.xslt
@@ -0,0 +1,13 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+  <xsl:output method="xml" indent="yes"/>
+
+  <!-- Remove remote event definitions -->
+  <xsl:template match="/configuration/sitecore/events/event[@name='item:saved:remote']"/>
+  
+  <xsl:template match="@* | node()">
+    <xsl:copy>
+      <xsl:apply-templates select="@* | node()"/>
+    </xsl:copy>
+  </xsl:template>
+</xsl:stylesheet>
---
+++ Website/Pipelines/CreateComment/DuplicateSubmissionGuard.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitecore.Diagnostics;
+using Sitecore.StringExtensions;
+using Sitecore.Modules.WeBlog.Items.WeBlog;
+
+namespace Sitecore.Modules.WeBlog.Pipelines.CreateComment
+{
+    public class DuplicateSubmissionGuard : ICreateCommentProcessor
+    {
+        public void Process(CreateCommentArgs args)
+        {
+            Assert.IsNotNull(args.Database, "Database cannot be null");
+            Assert.IsNotNull(args.Comment, "Comment cannot be null");
+            Assert.IsNotNull(args.EntryID, "Entry ID cannot be null");
+
+            var entryItem = args.Database.GetItem(args.EntryID, args.Language);
+            if (entryItem != null)
+            {
+                var query = "fast:{0}//*[@email='{1}' and @name='{2}' and @comment='{3}']".FormatWith(
+                    entryItem.Paths.FullPath, args.Comment.AuthorEmail, args.Comment.AuthorName, args.Comment.Text);
+                var comments = args.Database.SelectItems(query);
+
+                if(comments.Length > 0)
+                    args.AbortPipeline();
+            }
+        }
+    }
+}
