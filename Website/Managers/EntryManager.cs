6aec75348345147edf9e54b133cda473e4fb2086
Alistair Deneys <adn@sitecore.net>
2015-05-31 22:31:59 +1000

    Remove DLL which will be built by project. More fixes from merge conflicts

--- Website/Managers/EntryManager.cs
+++ Website/Managers/EntryManager.cs
@@ -295,7 +295,7 @@ namespace Sitecore.Modules.WeBlog.Managers
             }
 
             var searcher = new Searcher();
-            result = searcher.Execute<EntryItem>(query, maxNumber, (list, item) => list.Add((EntryItem)item), Constants.Index.Fields.EntryDate, true).ToList();
+            result = searcher.Execute<EntryItem>(query, blog.Language, maxNumber, (list, item) => list.Add((EntryItem)item), Constants.Index.Fields.EntryDate, true).ToList();
 #endif
             return result.ToArray();
         }
