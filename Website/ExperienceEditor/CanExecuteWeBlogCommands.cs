b6e6689a55079c1a01a551f07c789c758f6b2a3e
Alan NULL <alan-null@outlook.com>
2015-06-18 00:09:47 +0200

    #81 | WeBlog buttons from the ribbon are not visible in Sitecore 8.

---
+++ Website/ExperienceEditor/CanExecuteWeBlogCommands.cs
@@ -0,0 +1,20 @@
+﻿using Sitecore.Modules.WeBlog.Managers;
+using Sitecore.ExperienceEditor.Speak.Server.Contexts;
+using Sitecore.ExperienceEditor.Speak.Server.Requests;
+using Sitecore.ExperienceEditor.Speak.Server.Responses;
+
+namespace Sitecore.Modules.WeBlog.ExperienceEditor
+{
+    public class CanExecuteWeBlogCommands : PipelineProcessorRequest<ItemContext>
+    {
+        public override PipelineProcessorResponseValue ProcessRequest()
+        {
+            var currentItem = RequestContext.Item;
+            var currentBlog = ManagerFactory.BlogManagerInstance.GetCurrentBlog(currentItem);
+            return new PipelineProcessorResponseValue
+            {
+                Value = currentBlog != null
+            };
+        }
+    }
+}
---
+++ Website/ExperienceEditor/GetCurrentBlogId.cs
@@ -0,0 +1,18 @@
+﻿using Sitecore.Modules.WeBlog.Managers;
+using Sitecore.ExperienceEditor.Speak.Server.Contexts;
+using Sitecore.ExperienceEditor.Speak.Server.Requests;
+using Sitecore.ExperienceEditor.Speak.Server.Responses;
+
+namespace Sitecore.Modules.WeBlog.ExperienceEditor
+{
+    public class GetCurrentBlogId : PipelineProcessorRequest<ItemContext>
+    {
+        public override PipelineProcessorResponseValue ProcessRequest()
+        {
+            return new PipelineProcessorResponseValue
+            {
+                Value = ManagerFactory.BlogManagerInstance.GetCurrentBlog(RequestContext.Item).ID.Guid
+            };
+        }
+    }
+}
