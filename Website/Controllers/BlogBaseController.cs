f3db0d5f652986286c6abda08f902031a3e6b97b
Alan Płócieniak <alan-null@outlook.com>
2015-10-12 21:41:25 +0200

    #62 | BlogFeeds

--- Website/Controllers/BlogBaseController.cs
+++ Website/Controllers/BlogBaseController.cs
@@ -1,23 +1,27 @@
 ﻿using System.ComponentModel;
 using System.Web.Mvc;
+using Sitecore.Modules.WeBlog.Items.WeBlog;
 using Sitecore.Modules.WeBlog.layouts;
+using Sitecore.Modules.WeBlog.Managers;
 
 namespace Sitecore.Modules.WeBlog.Controllers
 {
     public class BlogBaseController : Controller
     {
+        private BlogHomeItem _currentBlog;
         public RenderingParameterHelper RenderingParameterHelper { get; set; }
+
+        [TypeConverter(typeof(Converters.ExtendedBooleanConverter))]
+        public bool VaryByBlog { get; set; }
+
+        public BlogHomeItem CurrentBlog
+        {
+            get { return _currentBlog ?? (_currentBlog = ManagerFactory.BlogManagerInstance.GetCurrentBlog()); }
+        }
 
         public BlogBaseController()
         {
             RenderingParameterHelper = new RenderingParameterHelper(this, true);
-        }
-
-        [TypeConverter(typeof(Converters.ExtendedBooleanConverter))]
-        public bool VaryByBlog
-        {
-            get;
-            set;
         }
     }
 }
---
+++ Website/Controllers/BlogFeedsController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+
+namespace Sitecore.Modules.WeBlog.Controllers
+{
+    public class BlogFeedsController : BlogBaseController
+    {
+        public ActionResult Index()
+        {
+            var feeds = CurrentBlog.SyndicationFeeds;
+            if (feeds != null)
+            {
+                return View("~/Views/WeBlog/Feeds.cshtml", feeds);
+            }
+            return null;
+        }
+    }
+}
