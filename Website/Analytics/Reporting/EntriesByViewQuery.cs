7706fd8fd8c3246accd004aa986b71c273d9aaba
Alistair Deneys <adn@sitecore.net>
2015-10-16 22:42:01 +1100

    Fixing whitespace

--- Website/Analytics/Reporting/EntriesByViewQuery.cs
+++ Website/Analytics/Reporting/EntriesByViewQuery.cs
@@ -8,63 +8,63 @@ using Sitecore.Data;
 
 namespace Sitecore.Modules.WeBlog.Analytics.Reporting
 {
-  /// <summary>
-  /// A reporting query to order Item IDs by views
-  /// </summary>
-  public class EntriesByViewQuery
+    /// <summary>
+    /// A reporting query to order Item IDs by views
+    /// </summary>
+    public class EntriesByViewQuery
 #if !SC75
-    : ItemBasedReportingQuery
+ : ItemBasedReportingQuery
 #endif
-  {
+    {
 #if SC75
     /// <summary>The <see cref="ReportDataProviderBase"/> to read reporting data from.</summary>
     private ReportDataProviderBase _reportProvider = null;
 #endif
 
-    /// <summary>
-    /// Creates a new instance of the class.
-    /// </summary>
-    /// <param name="reportProvider">The <see cref="ReportDataProviderBase"/> to read reporting data from.</param>
-    public EntriesByViewQuery(ReportDataProviderBase reportProvider = null)
+        /// <summary>
+        /// Creates a new instance of the class.
+        /// </summary>
+        /// <param name="reportProvider">The <see cref="ReportDataProviderBase"/> to read reporting data from.</param>
+        public EntriesByViewQuery(ReportDataProviderBase reportProvider = null)
 #if !SC75
-      : base(Constants.ReportingQueries.EntriesByView, reportProvider)
-    {
-    }
+            : base(Constants.ReportingQueries.EntriesByView, reportProvider)
+        {
+        }
 #else
     {
       _reportProvider = reportProvider ?? new ReportDataProvider();
     }
 #endif
 
-    /// <summary>
-    /// Gets or sets the IDs of the entries to get the visits for.
-    /// </summary>
-    public IEnumerable<ID> EntryIds { get; set; }
+        /// <summary>
+        /// Gets or sets the IDs of the entries to get the visits for.
+        /// </summary>
+        public IEnumerable<ID> EntryIds { get; set; }
 
-    /// <summary>
-    /// Gets the entry IDs ordered by visits.
-    /// </summary>
-    public IEnumerable<ID> OrderedEntryIds { get; protected set; }
+        /// <summary>
+        /// Gets the entry IDs ordered by visits.
+        /// </summary>
+        public IEnumerable<ID> OrderedEntryIds { get; protected set; }
 
-    /// <summary>
-    /// Executes the query against the reporting database.
-    /// </summary>
+        /// <summary>
+        /// Executes the query against the reporting database.
+        /// </summary>
 #if !SC75
-    public override void Execute()
-    {
-      var entryIdStr = string.Join("','", this.EntryIds);
+        public override void Execute()
+        {
+            var entryIdStr = string.Join("','", this.EntryIds);
 
-      var dt = this.ExecuteQuery(new Dictionary<string, object>
+            var dt = this.ExecuteQuery(new Dictionary<string, object>
       {
         { "@EntryIds", entryIdStr }
       });
 
-      if (dt != null)
-      {
-        OrderedEntryIds = from DataRow row in dt.Rows
-          select new ID((Guid) row["ItemId"]);
-      }
-    }
+            if (dt != null)
+            {
+                OrderedEntryIds = from DataRow row in dt.Rows
+                                  select new ID((Guid)row["ItemId"]);
+            }
+        }
 #else
     public void Execute()
     {
@@ -91,5 +91,5 @@ namespace Sitecore.Modules.WeBlog.Analytics.Reporting
       }
     }
 #endif
-  }
+    }
 }
