3dd685cff1de72a0e3d1caada642db297f91a307
Alan Płócieniak <alan-null@outlook.com>
2015-10-23 01:19:03 +0200

    #62 | EntryTags rendering

---
+++ Website/Controllers/BlogEntryTagsController.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Sitecore.Modules.WeBlog.Components.EntryTags;
+using Sitecore.Modules.WeBlog.Managers;
+using Sitecore.Mvc.Extensions;
+
+namespace Sitecore.Modules.WeBlog.Controllers
+{
+    public class BlogEntryTagsController : BlogBaseController
+    {
+        protected IEntryTagsCore EntryTagsCore { get; set; }
+
+        public BlogEntryTagsController() : this(null) { }
+
+        public BlogEntryTagsController(IEntryTagsCore entryTagsCore)
+        {
+            EntryTagsCore = entryTagsCore ?? new EntryTagsCore(CurrentBlog);
+        }
+
+        public ActionResult Index()
+        {
+            Dictionary<string, string> model = new Dictionary<string, string>();
+            if (!Context.PageMode.IsPageEditorEditing)
+            {
+                var tags = ManagerFactory.TagManagerInstance.GetTagsByEntry(CurrentEntry);
+                model = tags.ToDictionary(t => t.Key, t => EntryTagsCore.GetTagUrl(t.Key));
+            }
+            return View("~/Views/WeBlog/EntryTags.cshtml", model);
+        }
+    }
+}
