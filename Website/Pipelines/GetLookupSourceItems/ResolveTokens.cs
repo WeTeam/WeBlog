3a6f76360a0ffe76a9d6ec3b29acdf965a521c79
Alan Płócieniak <alan-null@outlook.com>
2015-11-30 21:50:08 +0100

    TokenReplacer

---
+++ Website/Pipelines/GetLookupSourceItems/ResolveTokens.cs
@@ -0,0 +1,15 @@
+﻿using Sitecore.Pipelines.GetLookupSourceItems;
+
+namespace Sitecore.Modules.WeBlog.Pipelines.GetLookupSourceItems
+{
+    public class ResolveTokens
+    {
+        public void Process(GetLookupSourceItemsArgs args)
+        {
+            if (args.Source.Contains(Constants.Tokens.WeBlogContext))
+            {
+                args.Source = TokenReplacer.ResolveContextTokens(args.Source, args.Item);
+            }
+        }
+    }
+}
