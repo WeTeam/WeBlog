5f8ac624ce35f9a63f5b98c147796f63bcdfefb2
Alan Płócieniak <alan-null@outlook.com>
2015-12-01 23:56:03 +0100

    #110 | Category based bucket path

---
+++ Website/Buckets/CategoryBucketFolderPath.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using Sitecore.Buckets.Util;
+using Sitecore.Data;
+using Sitecore.Data.Fields;
+using Sitecore.Data.Items;
+
+namespace Sitecore.Modules.WeBlog.Buckets
+{
+    public class CategoryBucketFolderPath : IDynamicBucketFolderPath
+    {
+        public string FieldName { set; get; }
+        public string Default { set; get; }
+
+        [Obsolete]
+        public string GetFolderPath(ID newItemId, ID parentItemId, DateTime creationDateOfNewItem)
+        {
+            return GetFolderPath(Context.ContentDatabase, null, null, newItemId, parentItemId, creationDateOfNewItem);
+        }
+
+        public string GetFolderPath(Database database, string name, ID templateId, ID newItemId, ID parentItemId, DateTime creationDateOfNewItem)
+        {
+            var innerItem = database.GetItem(newItemId);
+            if (innerItem != null)
+            {
+                var field = (MultilistField)innerItem.Fields[FieldName];
+                if (field != null)
+                {
+                    var firstCategory = GetFirstCategoryItem(field);
+                    if (firstCategory != null)
+                    {
+                        return firstCategory.Name;
+                    }
+                }
+            }
+            return Default ?? "Uncategorized";
+        }
+
+        private static Item GetFirstCategoryItem(MultilistField field)
+        {
+            return field.GetItems().FirstOrDefault();
+        }
+    }
+}
