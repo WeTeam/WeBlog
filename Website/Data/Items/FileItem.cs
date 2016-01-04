2f5844ad1e41afd3b12adff8f305e6baa48deebf
Alistair Deneys <adn@sitecore.net>
2016-01-04 23:49:54 +1100

    Implement new theme system

---
+++ Website/Data/Items/FileItem.cs
@@ -0,0 +1,33 @@
+﻿using Sitecore.Data.Fields;
+using Sitecore.Data.Items;
+
+namespace Sitecore.Modules.WeBlog.Data.Items
+{
+    public class FileItem : CustomItem
+    {
+        public FileItem(Item innerItem) : base(innerItem) { }
+
+        public static implicit operator FileItem(Item innerItem)
+        {
+            return innerItem != null ? new FileItem(innerItem) : null;
+        }
+
+        public static implicit operator Item(FileItem customItem)
+        {
+            return customItem != null ? customItem.InnerItem : null;
+        }
+
+        public string Url
+        {
+            get
+            {
+                return InnerItem[Fields.Url];
+            }
+        }
+
+        public static class Fields
+        {
+            public const string Url = "Url";
+        }
+    }
+}
--- Website/Data/Items/ThemeItem.cs
+++ Website/Data/Items/ThemeItem.cs
@@ -1,11 +1,20 @@
+using System.Collections.Generic;
 using Sitecore.Data.Items;
 using Sitecore.Modules.WeBlog.Data.Fields;
+using Sitecore.Modules.WeBlog.Extensions;
 
 namespace Sitecore.Modules.WeBlog.Data.Items
 {
     public class ThemeItem : CustomItem
     {
-        public ThemeItem(Item innerItem) : base(innerItem) { }
+        public IEnumerable<FileItem> Stylesheets { get; protected set; }
+
+        public IEnumerable<ScriptItem> Scripts { get; protected set; }
+
+        public ThemeItem(Item innerItem) : base(innerItem)
+        {
+            ResolveThemeAssets();
+        }
 
         public static implicit operator ThemeItem(Item innerItem)
         {
@@ -17,9 +26,30 @@ namespace Sitecore.Modules.WeBlog.Data.Items
             return customItem != null ? customItem.InnerItem : null;
         }
 
+        // to be removed once 2016 themes are done
         public CustomTextField FileLocation
         {
             get { return new CustomTextField(InnerItem, InnerItem.Fields["File Location"]); }
         }
+
+        protected void ResolveThemeAssets()
+        {
+            var scriptList = new List<ScriptItem>();
+            var stylesheetList = new List<FileItem>();
+
+            var children = InnerItem.GetChildren();
+
+            foreach(Item item in children)
+            {
+                if (item.TemplateIsOrBasedOn(ScriptItem.TemplateId))
+                    scriptList.Add(new ScriptItem(item));
+
+                if (item.TemplateIsOrBasedOn(StylesheetItem.TemplateId))
+                    stylesheetList.Add(new StylesheetItem(item));
+            }
+
+            Scripts = scriptList;
+            Stylesheets = stylesheetList;
+        }        
     }
 }
