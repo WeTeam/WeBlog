0d94e9d2d8653481cec2a5fc4f987cf66809cfa5
Alistair Deneys <adn@sitecore.net>
2016-05-31 21:31:26 +1000

    Migrated BlogManager tests to unit test project

--- Website/Managers/BlogManager.cs
+++ Website/Managers/BlogManager.cs
@@ -5,6 +5,7 @@ using Sitecore.Data.Items;
 using Sitecore.Modules.WeBlog.Extensions;
 using Sitecore.Modules.WeBlog.Data.Items;
 using Sitecore.Security.Accounts;
+using Sitecore.Modules.WeBlog.Configuration;
 
 namespace Sitecore.Modules.WeBlog.Managers
 {
@@ -13,6 +14,20 @@ namespace Sitecore.Modules.WeBlog.Managers
     /// </summary>
     public class BlogManager : IBlogManager
     {
+        /// <summary>
+        /// The <see cref="IWeBlogSettings"/> to access settings from.
+        /// </summary>
+        protected IWeBlogSettings Settings = null;
+
+        /// <summary>
+        /// Creates a new instance.
+        /// </summary>
+        /// <param name="settings">The settings to use. If null, the default settings will be used.</param>
+        public BlogManager(IWeBlogSettings settings = null)
+        {
+            Settings = settings ?? new WeBlogSettings();
+        }
+
         /// <summary>
         /// Gets the current blog for the context item
         /// </summary>
@@ -29,7 +44,7 @@ namespace Sitecore.Modules.WeBlog.Managers
         /// <returns>The current blog if found, otherwise null</returns>
         public BlogHomeItem GetCurrentBlog(Item item)
         {
-            var blogItem = item.FindAncestorByTemplate(Settings.BlogTemplateIDString);
+            var blogItem = item.FindAncestorByAnyTemplate(Settings.BlogTemplateIds);
 
             if (blogItem != null)
                 return new BlogHomeItem(blogItem);
@@ -63,16 +78,19 @@ namespace Sitecore.Modules.WeBlog.Managers
         /// </summary>
         /// <param name="database">The database to get the blogs from. If null, use the context database</param>
         /// <returns>The list of all blogs</returns>
-        public BlogHomeItem[] GetAllBlogs(Database database)
+        public BlogHomeItem[] GetAllBlogs(Database database = null)
         {
             if (database == null)
                 database = Context.Database;
 
-            // TODO: Store the result of this call in cache and clear it from cache on publish
-            var blogTemplate = database.GetTemplate(Settings.BlogTemplateIDString);
             var contentRoot = database.GetItem(Settings.ContentRootPath);
-            var blogItems = contentRoot.FindItemsByTemplateOrDerivedTemplate(blogTemplate);
-            return (from item in blogItems select new BlogHomeItem(item)).ToArray();
+
+            return (from templateId in Settings.BlogTemplateIds
+                   let template = database.GetTemplate(templateId)
+                   let blogItems = contentRoot.FindItemsByTemplateOrDerivedTemplate(template)
+                   from item in blogItems
+                   where item != null
+                   select new BlogHomeItem(item)).ToArray();
         }
 
         /// <summary>
@@ -92,6 +110,9 @@ namespace Sitecore.Modules.WeBlog.Managers
         /// <returns>True if RSS is enabled, otherwise False</returns>
         public bool EnableRSS(BlogHomeItem blog)
         {
+            if (blog == null)
+                return false;
+
             return blog.EnableRss.Checked;
         }
 
@@ -112,6 +133,9 @@ namespace Sitecore.Modules.WeBlog.Managers
         /// <returns>True if email should be shown, otherwise False</returns>
         public bool ShowEmailWithinComments(BlogHomeItem blog)
         {
+            if (blog == null)
+                return false;
+
             return blog.ShowEmailWithinComments.Checked;
         }
 
