8fd5c7e6c8ba2b0754815cf883b7cfce39fe574c
Alistair Deneys <adn@sitecore.net>
2016-08-02 22:08:53 +1000

    Rename legacy test project to IntegrationTest

---
+++ IntegrationTest/CustomBlog/CommentManager.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+
+namespace Sitecore.Modules.WeBlog.Test.CustomBlog
+{
+    [TestFixture]
+    [Category("CustomBlog.CommentManager")]
+    public class CommentManager : Sitecore.Modules.WeBlog.Test.CommentManager
+    {
+        [TestFixtureSetUp]
+        public void ChangeBlog()
+        {
+            Sitecore.Context.Database.SetupCustomBlogs(TestContentRoot);
+            //re-init to retrieve member items
+            Initialize();
+        }
+
+        [TestFixtureTearDown]
+        public void RemoveTemplates()
+        {
+            Sitecore.Context.Database.RemoveCustomTemplates();
+        }
+    }
+}
