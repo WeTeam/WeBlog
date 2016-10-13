145c4dad4c51c06939940b30cea6a8939d60e8e0
Alistair Deneys <adn@sitecore.net>
2016-10-13 21:54:04 +1100

    Extract common assembly properties to shared solution info file

---
+++ src/SolutionInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("WeTeam community")]
+[assembly: AssemblyProduct("WeBlog")]
+[assembly: AssemblyCopyright("Copyright ©  2011 - 2016")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(false)]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("3.0.0.*")]
+[assembly: AssemblyFileVersion("3.0.0.0")]
